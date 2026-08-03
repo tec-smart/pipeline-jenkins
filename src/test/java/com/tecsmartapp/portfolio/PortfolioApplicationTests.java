1ea94946dbefef82a71390b3c7626e1497263bc3
tec-smart <contact@tecsmartapp.com>
2026-08-03 15:45:25 -0500

    cambio el test

--- src/test/java/com/tecsmartapp/portfolio/PortfolioApplicationTests.java
+++ src/test/java/com/tecsmartapp/portfolio/PortfolioApplicationTests.java
@@ -1,12 +1,8 @@
 package com.tecsmartapp.portfolio;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.sql.Connection;
-
 import javax.sql.DataSource;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,22 +11,14 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")
 class DatabaseConnectionTest {
-
     @Autowired
     private DataSource dataSource;
-
     @Test
     void shouldConnectToDatabase() throws Exception {
-
         assertNotNull(dataSource);
-
         try (Connection connection = dataSource.getConnection()) {
-
             assertNotNull(connection);
             assertTrue(connection.isValid(2));
-
         }
-
     }
-
 }
