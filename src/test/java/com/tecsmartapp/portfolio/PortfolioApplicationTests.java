e91d380c7b3fd7ad25bf7815530f01520c6eb764
tec-smart <contact@tecsmartapp.com>
2026-08-03 17:06:59 -0500

    implemento test

--- src/test/java/com/tecsmartapp/portfolio/PortfolioApplicationTests.java
+++ src/test/java/com/tecsmartapp/portfolio/PortfolioApplicationTests.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.sql.Connection;
 import javax.sql.DataSource;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,16 +13,18 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class DatabaseConnectionTest {
+class PortfolioApplicationTests {
+
     @Autowired
     private DataSource dataSource;
 
     @Test
-    void shouldConnectToDatabase() throws Exception {
-        assertNotNull(dataSource);
+    @DisplayName("Should load Spring context and connect to PostgreSQL")
+    void shouldLoadContextAndConnectToDatabase() throws Exception {
+        assertNotNull(dataSource, "DataSource should be created");
         try (Connection connection = dataSource.getConnection()) {
-            assertNotNull(connection);
-            assertTrue(connection.isValid(2));
+            assertNotNull(connection, "Connection should not be null");
+            assertTrue(connection.isValid(2), "Connection should be valid");
         }
     }
 }
